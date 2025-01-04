1394b558627773e49e17cb3057c18edea7de7d68
John Uriarte <uriarte.johnlennon@gmail.com>
2025-01-04 02:24:52 -0500

    add item add, remove, update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { addItem, removeItem, updateQuantity } from "./cartSlice";
+import { addItem } from "./cartSlice";
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
